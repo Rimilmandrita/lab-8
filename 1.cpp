46d884a02d85c7dc7ebf6bfebf8c3cfae62b86e3
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-11-17 23:31:50 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -79,5 +79,83 @@ int main() {
 	a.addNode(3);
 	a.addNode(4);
 	a.displayList();
-		return 0;
+	#include <iostream>
+using namespace std;
+
+struct node
+{
+	int data;
+	node *next;
+};
+class linkedList
+{private:
+node *head, *tail;
+public :
+linkedList()
+{ head=NULL;
+  tail=NULL;}
+void addNode(int n)
+{node *tmp = new node;
+tmp->data=n;
+tmp->next=NULL;
+
+if(head==NULL)
+{ head=tmp;
+  tail=tmp;}
+else
+{
+	tail->next=tmp;
+	tail= tmp;
+}
+}
+void displayList()
+{ if (head==NULL)
+cout<<"The linked list is empty";
+  else
+ { node *tmp= head;
+ cout<<"The list is:";
+  while(tmp!=NULL)
+  {cout<<tmp->data<<" ";
+    tmp=tmp->next;
+  	}
+  	}
+}
+void insert(int pos,int n1)
+{
+    node *prev,*cur;
+    node *tmp=new node;
+    tmp->data=n1;
+    tmp->next=NULL;
+    prev=NULL;
+    cur=head;
+    int count=1;
+   
+        while(count!=pos)
+        {
+            prev=cur;
+            cur=cur->next;
+            count++;
+        }
+        if(count==pos)
+        {
+            prev->next=tmp;
+            tmp->next=cur;
+        }
+        else
+            cout<<"\nNot Able to Insert";
+          }
+};
+int main() {
+	linkedList a;
+	int n1, pos;
+	a.addNode(3);
+	a.addNode(4);
+	cout<<"Enter the element to be inserted";
+	cin>>n1;
+	cout<<"\nEnter The position to insert\n";
+	cin>>pos;
+	a.insert(pos,n1);
+	a.displayList();
+	return 0;
+}	return 0;
 }
